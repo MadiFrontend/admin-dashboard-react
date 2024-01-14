4dcff53721c47e0ee43f9f77b0b71d7e88f8766a
madifrontend <madifrontend@gmail.com>
2024-01-14 13:00:16 +0330

    update app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import Login from "./features/identity/component/Login";
+import Register from "./features/identity/component/register";
+
+function App() {
+  return (
+    <>
+      <Register />
+    </>
+  );
+}
+
+export default App;
