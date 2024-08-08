b7ed4f889f57a97393bf1cbb5dc50388ba31dee2
madifrontend <madifrontend@gmail.com>
2024-08-08 14:42:23 +0330

    change BaseURL

--- src/core/http-service.js
+++ src/core/http-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://react-mini-project-api.classbon.com";
+const BASE_URL = "https://react-mini-projects-api.classbon.com";
 
 export const httpService = axios.create({
   baseURL: BASE_URL,
